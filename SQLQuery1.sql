33a551cd6084fc8d63a4644ddce12a65513dd8fd
Kate128 <112573039+Kate128@users.noreply.github.com>
2024-10-31 10:01:10 +0200

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE online_store;
+
+USE online_store;
+
+CREATE TABLE id_counter (
+    id INT PRIMARY KEY
+);
+INSERT INTO id_counter (id) VALUES (0);
+
+CREATE TRIGGER before_insert_products
+BEFORE INSERT ON products
+FOR EACH ROW
+BEGIN
+    UPDATE id_counter SET id = LAST_INSERT_ID(id + 1);
+    SET NEW.id = LAST_INSERT_ID();
+END;
+CREATE TABLE products (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    image VARCHAR(255)
+);
+CREATE TABLE orders (
+    id INT PRIMARY KEY,  -- Звичайний INT
+    total DECIMAL(10, 2) NOT NULL,
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE order_items (
+    id INT PRIMARY KEY,  -- Звичайний INT
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
